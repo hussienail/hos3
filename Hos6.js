1c05b7c15e9a67276b4f7dce8a195f3d473cfa6b
hussienail <63175073+hussienail@users.noreply.github.com>
2020-08-11 22:28:44 +0300

    Update and rename Hos7.js to Hos6.js

--- Hos6.js
+++ Hos6.js
@@ -20,7 +20,7 @@ clearInterval(myVar);
 })
 
 $('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
-<center><iframe src="https://mixlr.com/%D8%A7%D8%B0%D8%A7%D8%B9%D9%87-%D8%B4%D8%A7%D8%AA-%D8%A7%D9%88%D8%AA%D8%A7%D8%B1-%D8%A8%D8%BA%D8%AF%D8%A7%D8%AF?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
+<center><iframe src="https://mixlr.com/اذاعه-شات-اوتار-بغداد?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
 <center></center></center>
 </div>`);
 $('#d0').append(`<label title="الإذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
