604456be5fdce68d9a0e6ea8b06a12e8fd5285d3
hussienail <63175073+hussienail@users.noreply.github.com>
2020-08-15 18:25:12 +0300

    Update and rename Hoos.js to Hoos1.js

--- Hoos1.js
+++ Hoos1.js
@@ -20,7 +20,7 @@ clearInterval(myVar);
 })
 
 $('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
-<center><iframe src="https://mixlr.com/kawray-kon?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
+<center><iframe src="https://mixlr.com/kawray-kon/embed/?autoplay=true" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
 <center></center></center>
 </div>`);
 $('#d0').append(`<label title="الإذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
